a25223be1a5497863354eb33547db5a1bd1be221
carlos alonso barahona alvarez <carlosalonsobarahona261@gmail.com>
2026-03-14 10:26:48 -0500

    primer commit con la creacion del proyecto springboot

---
+++ src/main/java/com/edu/uniquindio/RuralStay/RuralStayApplication.java
@@ -0,0 +1,13 @@
+package com.edu.uniquindio.RuralStay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RuralStayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RuralStayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edu/uniquindio/RuralStay/RuralStayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edu.uniquindio.RuralStay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuralStayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
