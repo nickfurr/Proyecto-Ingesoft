2e191e5bb8f2f13121306fe045ff7b5962778a41
MigueBetancur <miguela.betancurr@uqvirtual.edu.co>
2026-05-13 12:41:22 -0500

    fix: Se corrigió el test crearCasaRural

--- src/test/java/com/edu/uniquindio/ruralstay/CrearCasaServiceTest.java
+++ src/test/java/com/edu/uniquindio/ruralstay/CrearCasaServiceTest.java
@@ -1,8 +1,8 @@
 package com.edu.uniquindio.ruralstay;
 
-import com.edu.uniquindio.ruralstay.dto.CasaRuralDTO;
-import com.edu.uniquindio.ruralstay.dto.CrearCasaRuralDTO;
+import com.edu.uniquindio.ruralstay.dto.*;
 import com.edu.uniquindio.ruralstay.entity.Propietario;
+import com.edu.uniquindio.ruralstay.entity.enums.TipoCama;
 import com.edu.uniquindio.ruralstay.repository.PropietarioRepository;
 import com.edu.uniquindio.ruralstay.service.CasaRuralService;
 import org.junit.jupiter.api.Test;
@@ -38,19 +38,78 @@ public class CrearCasaServiceTest {
 
         propietario = propietarioRepository.save(propietario);
 
-        // DTO
+        // Crear habitaciones
+        HabitacionDTO habitacion1 = new HabitacionDTO(
+                null,
+                3,
+                TipoCama.DOBLE,
+                true
+        );
+
+        HabitacionDTO habitacion2 = new HabitacionDTO(
+                null,
+                3,
+                TipoCama.SENCILLA,
+                false
+        );
+
+        HabitacionDTO habitacion3 = new HabitacionDTO(
+                null,
+                3,
+                TipoCama.SENCILLA,
+                true
+        );
+
+        // Crear baños
+        BañoDTO bano1 = new BañoDTO(
+                null,
+                true
+        );
+
+        BañoDTO bano2 = new BañoDTO(
+                null,
+                false
+        );
+
+        // Crear cocinas
+        CocinaDTO cocina1 = new CocinaDTO(
+                null,
+                true,
+                true
+        );
+
+        // DTO principal
         CrearCasaRuralDTO dto = new CrearCasaRuralDTO();
+
         dto.setPoblacion("Armenia");
         dto.setDescripcionGeneral("Casa bonita");
-        dto.setNumeroDormitorios(3);
-        dto.setNumeroBanos(2);
-        dto.setNumeroCocinas(1);
+
+        dto.setHabitaciones(List.of(
+                habitacion1,
+                habitacion2,
+                habitacion3
+        ));
+
+        dto.setBanos(List.of(
+                bano1,
+                bano2
+        ));
+
+        dto.setCocinas(List.of(
+                cocina1
+        ));
+
         dto.setNumeroComedores(1);
         dto.setPlazasGaraje(1);
+
         dto.setFotos(List.of(
                 "https://planner5d.com/blog/es/como-construir-casa/"
         ));
+
         dto.setPropietarioId(propietario.getId());
+
+        dto.setCiudad("Armenia");
+        dto.setPrecio(250000.0);
 
         // Ejecutar
         CasaRuralDTO resultado = casaRuralService.crearCasa(dto);
